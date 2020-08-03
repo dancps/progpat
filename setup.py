f825bfa028121deafb51f1bf1f335f84ffe209b4
Danilo <dancps96@gmail.com>
2020-08-03 16:48:08 -0300

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='progpat',
+    version='0.1',
+    license='MIT License',
+    long_description=open('README.md').read(),
+    url='https://github.com/dancps/progpat',
+    author='Danilo Calhes',
+    #package_dir={'': 'progpat'},
+    packages=find_packages(),
+    entry_points={'console_scripts': ['progpat=progpat.progpat:main',],},
+)
 # do the ln
 # add to path
